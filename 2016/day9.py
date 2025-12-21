cc5c4c727e787271709f7141b99449d7b61b17ac
Cem Alpturk <cem.alpturk@gmail.com>
2025-12-21 23:29:40 +0100

    day9

---
+++ 2016/day9.py
@@ -0,0 +1,92 @@
+import os
+import time
+
+
+def read_input(filename: str) -> list[str]:
+    dir = os.path.basename(__file__).split(".")[0]
+
+    filepath = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "data",
+            dir,
+            filename,
+        )
+    )
+
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+    return [line.strip("\n") for line in lines]
+
+
+def part1():
+    # filename = "part1-test.txt"
+    filename = "part1.txt"
+
+    lines = read_input(filename)
+    c = 0 
+    
+    for line in lines:
+        start = 0
+        while True:
+            # print(line)
+            start = line.find("(", start)
+            if start == -1:
+                break
+            end = line.find(")", start)
+            # print(line[start: end])
+            marker = line[start+1:end]
+            n, k = map(int, marker.split("x"))
+            data = line[end+1:end+n+1] * k
+            line = line[:start] + data + line[end+n+1:]
+            start = start + n * k 
+            # print(len(line), line)
+        c += len(line)
+
+    return c
+
+
+def decompress(s: str) -> int:
+    start = 0
+    tot = len(s)
+    while True:
+        start = s.find("(", start)
+        if start == -1:
+            break
+        end = s.find(")", start)
+        marker = s[start+1:end]
+        n, k = map(int, marker.split("x"))
+        n_data = decompress(s[end+1:end+n+1]) * k
+        tot -= end - start + 1
+        tot -= n
+        tot += n_data
+        # s = s[:start] + data + s[end+n+1:]
+        start = end + n + 1
+    
+    return tot 
+
+
+def part2():
+    # filename = "part2-test.txt"
+    filename = "part2.txt"
+
+    lines = read_input(filename)
+    c = 0 
+
+    for line in lines:
+        s = decompress(line)
+        c += s
+    return c
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    result1 = part1()
+    elapsed1 = time.perf_counter() - start
+    print(f"Part1: {result1} (Time: {elapsed1:.6f}s)")
+
+    start = time.perf_counter()
+    result2 = part2()
+    elapsed2 = time.perf_counter() - start
+    print(f"Part2: {result2} (Time: {elapsed2:.6f}s)")
