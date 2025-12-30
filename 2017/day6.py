f1404ec430f7c2ba70af04e11586cfcf941a84b2
Cem Alpturk <cem.alpturk@gmail.com>
2025-12-30 11:41:02 +0100

    day6

---
+++ 2017/day6.py
@@ -0,0 +1,97 @@
+import os
+import time
+
+
+def read_input(filename: str) -> list[str]:
+    dir = os.path.basename(__file__).split(".")[0]
+
+    filepath = os.path.abspath(
+        os.path.join(
+            os.path.dirname(__file__),
+            "data",
+            dir,
+            filename,
+        )
+    )
+
+    with open(filepath, "r") as f:
+        lines = f.readlines()
+
+    return [line.strip("\n") for line in lines]
+
+
+def part1():
+    # filename = "part1-test.txt"
+    filename = "part1.txt"
+
+    lines = read_input(filename)
+    blocks = list(map(int, lines[0].split("\t")))
+    n = len(blocks)
+    seen = set()
+    i = 0
+    while tuple(blocks) not in seen:
+        seen.add(tuple(blocks))
+        idx = blocks.index(max(blocks))
+        v = blocks[idx]
+        blocks[idx] = 0 
+
+        while v > 0:
+            idx = (idx + 1) % n 
+            blocks[idx] += 1 
+            v -= 1 
+
+        i += 1 
+
+    return i
+
+def part2():
+    # filename = "part2-test.txt"
+    filename = "part1.txt"
+
+    lines = read_input(filename)
+    blocks = list(map(int, lines[0].split("\t")))
+    n = len(blocks)
+    seen = set()
+    i = 0
+    while tuple(blocks) not in seen:
+        seen.add(tuple(blocks))
+        idx = blocks.index(max(blocks))
+        v = blocks[idx]
+        blocks[idx] = 0 
+
+        while v > 0:
+            idx = (idx + 1) % n 
+            blocks[idx] += 1 
+            v -= 1 
+
+        i += 1 
+
+    i = 0 
+    seen = set()
+    while tuple(blocks) not in seen:
+        seen.add(tuple(blocks))
+        idx = blocks.index(max(blocks))
+        v = blocks[idx]
+        blocks[idx] = 0 
+
+        while v > 0:
+            idx = (idx + 1) % n 
+            blocks[idx] += 1 
+            v -= 1 
+
+        i += 1 
+
+    return i
+
+
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    result1 = part1()
+    elapsed1 = time.perf_counter() - start
+    print(f"Part1: {result1} (Time: {elapsed1:.6f}s)")
+
+    start = time.perf_counter()
+    result2 = part2()
+    elapsed2 = time.perf_counter() - start
+    print(f"Part2: {result2} (Time: {elapsed2:.6f}s)")
